df895814a3df3022f7cf96162a3d0b758e5961fc
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-08 23:06:39 +0530

    INSTRUCTION_DUPLICATE

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wshadow"
-#pragma clang diagnostic pop
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdint.h>
 #include <assert.h>
 #include <string.h>
+#include <errno.h>
 
 #define ARRAY_SIZE(xs) (sizeof(xs) / sizeof((xs)[0]))
 #define VM_STACK_CAPACITY 1024
@@ -20,6 +17,7 @@ typedef enum {
   TRAP_ILLEGAL_INSTRUCTION,
   TRAP_ILLEGAL_INSTRUCTION_ACCESS,
   TRAP_ILLEGAL_OPERAND_ZERO,
+  TRAP_ILLEGAL_OPERAND
 } Trap;
 
 // Trap Textual Representation
@@ -37,6 +35,8 @@ const char *trap_as_cstr(Trap trap) {
       return "TRAP_ILLEGAL_INSTRUCTION_ACCESS";
     case TRAP_ILLEGAL_OPERAND_ZERO:
       return "TRAP_ILLEGAL_OPERAND_ZERO";
+    case TRAP_ILLEGAL_OPERAND:
+      return "TRAP_ILLEGAL_OPERAND";
     default:
       assert(0 && "trap_as_cstr: Unreachable");
   }
@@ -44,6 +44,7 @@ const char *trap_as_cstr(Trap trap) {
 
 typedef enum {
   INSTRUCTION_PUSH,
+  INSTRUCTION_DUPLICATE,
   INSTRUCTION_PLUS,
   INSTRUCTION_MINUS,
   INSTRUCTION_MULTIPLY,
@@ -77,6 +78,8 @@ const char *instruction_type_as_cstr(InstructionType type) {
       return "INSTRUCTION_HALT";
     case  INSTRUCTION_PRINT_DEBUG:
       return " INSTRUCTION_PRINT_DEBUG";
+    case INSTRUCTION_DUPLICATE:
+      return "INSTRUCTION_DUPLICATE";
     default:
       assert(0 && "instruction_type_as_cstr: Unreachable");
   }
@@ -102,6 +105,8 @@ typedef struct {
 } VM;
 
 #define MAKE_INSTRUCTION_PUSH(value) {.type = INSTRUCTION_PUSH, .operand = (value)}
+#define MAKE_INSTRUCTION_DUPLICATE(address) {.type = INSTRUCTION_DUPLICATE, \
+.operand = (address)}
 #define MAKE_INSTRUCTION_PLUS() {.type = INSTRUCTION_PLUS}
 #define MAKE_INSTRUCTION_MINUS() {.type = INSTRUCTION_MINUS}
 #define MAKE_INSTRUCTION_MULTIPLY() {.type = INSTRUCTION_MULTIPLY}
@@ -131,12 +136,14 @@ Trap vm_execute_instruction(VM *vm) {
     
     case INSTRUCTION_PUSH:
       if (vm->stackSize >= VM_STACK_CAPACITY) return TRAP_STACK_OVERFLOW;
+      
       vm->stack[vm->stackSize++] = instruction.operand;
       vm->instructionPointer += 1;
       break;
     
     case INSTRUCTION_PLUS:
       if (vm->stackSize < 2) return TRAP_STACK_UNDERFLOW;
+      
       vm->stack[vm->stackSize - 2] += vm->stack[vm->stackSize - 1];
       vm->stackSize -= 1;
       vm->instructionPointer += 1;
@@ -151,6 +158,7 @@ Trap vm_execute_instruction(VM *vm) {
     
     case INSTRUCTION_MULTIPLY:
       if (vm->stackSize < 2) return TRAP_STACK_UNDERFLOW;
+      
       vm->stack[vm->stackSize - 2] *= vm->stack[vm->stackSize - 1];
       vm->stackSize -= 1;
       vm->instructionPointer += 1;
@@ -159,6 +167,7 @@ Trap vm_execute_instruction(VM *vm) {
     case INSTRUCTION_DIVIDE:
       if (vm->stackSize < 2) return TRAP_STACK_UNDERFLOW;
       if (vm->stack[vm->stackSize - 2] == 0) return TRAP_ILLEGAL_OPERAND_ZERO;
+      
       vm->stack[vm->stackSize - 2] /= vm->stack[vm->stackSize - 1];
       vm->stackSize -= 1;
       vm->instructionPointer += 1;
@@ -170,9 +179,12 @@ Trap vm_execute_instruction(VM *vm) {
     
     case INSTRUCTION_JUMP_IF:
       if (vm->stackSize < 1) return TRAP_STACK_UNDERFLOW;
+      
       if (vm->stack[vm->stackSize - 1] == 1) {
         vm->stackSize -= 1;
         vm->instructionPointer = instruction.operand;
+      } else {
+        vm->instructionPointer += 1;
       }
       break;
     
@@ -182,6 +194,7 @@ Trap vm_execute_instruction(VM *vm) {
       
     case INSTRUCTION_EQUALITY:
       if (vm->stackSize < 2) return TRAP_STACK_UNDERFLOW;
+      
       vm->stack[vm->stackSize - 2] = vm->stack[vm->stackSize - 1] ==
           vm->stack[vm->stackSize - 2];
       vm->stackSize -= 1;
@@ -190,12 +203,23 @@ Trap vm_execute_instruction(VM *vm) {
       
     case INSTRUCTION_PRINT_DEBUG:
       if (vm->stackSize < 1) return TRAP_STACK_UNDERFLOW;
+      
       printf("%ld\n", vm->stack[vm->stackSize - 1]);
       vm->stackSize -= 1;
       vm->instructionPointer += 1;
-      
       break;
       
+    case INSTRUCTION_DUPLICATE:
+      if (vm->stackSize >= VM_STACK_CAPACITY) return TRAP_STACK_OVERFLOW;
+      if (vm->stackSize - instruction.operand <= 0) return TRAP_STACK_UNDERFLOW;
+      if (instruction.operand < 0) return TRAP_ILLEGAL_OPERAND;
+      
+      vm->stack[vm->stackSize] = vm->stack[vm->stackSize - 1 - instruction
+      .operand];
+      vm->stackSize += 1;
+      vm->instructionPointer += 1;
+      break;
+    
     default:
       return TRAP_ILLEGAL_INSTRUCTION;
   }
@@ -206,21 +230,13 @@ void vm_dump_stack(FILE *stream, const VM *vm) {
   fprintf(stream, "Stack:\n");
   if (vm->stackSize > 0) {
     for (Word i = 0; i < vm->stackSize; ++i) {
-      fprintf(stream, "  %ld\n", vm->stack[i]);
+      fprintf(stream, "%ld ", vm->stack[i]);
     }
+    printf("\n");
   } else {
     fprintf(stream, "  [Stack Empty]\n");
   }
 }
-
-
-VM vm = {0};
-Instruction program[] = {
-    MAKE_INSTRUCTION_PUSH(100), // 0
-    MAKE_INSTRUCTION_PUSH(1),  // 1
-    MAKE_INSTRUCTION_PLUS(),    // 2
-    MAKE_INSTRUCTION_JUMP(1)    // 3
-};
 
 void vm_load_program_from_memory(
     VM *vm,
@@ -243,11 +259,13 @@ int main() {
            instruction_type_as_cstr(program[vm.instructionPointer].type));
     Trap trap = vm_execute_instruction(&vm);
     vm_dump_stack(stdout, &vm);
+    
     if (trap != TRAP_OK) {
       fprintf(stderr, "Trap Activated: %s\n", trap_as_cstr(trap));
       vm_dump_stack(stderr, &vm);
       exit(1);
     }
   }
+  vm_dump_stack(stdout, &vm);
   return 0;
 }
